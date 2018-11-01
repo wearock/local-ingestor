62a2d9b7bd938409e48e66b546a9cb331172c8fc
Justin Jia <jibin.jia@demandforce.com>
2018-11-01 15:56:18 +0800

    Add default test account for Vivi & Serena

--- settings/db/initdata.sql
+++ settings/db/initdata.sql
@@ -1,9 +1,1 @@
 insert into ibex.server(name,host_name,http_port,https_port,default_scheme) values ('Upload','10.20.0.95','9999','9999','http');
-insert into ibex.client_feature(client_id,feature_name,is_enabled) values
-    (1,'pms_writeback',true),
-    (2,'pms_writeback',true),
-    (3,'pms_writeback',true),
-    (4,'pms_writeback',true),
-    (5,'pms_writeback',true),
-    (6,'pms_writeback',true),
-    (7,'pms_writeback',true);
