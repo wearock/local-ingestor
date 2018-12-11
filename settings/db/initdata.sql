0b1820a8a1420463cd2cd653448a8aba7193b5cf
Justin Jia <jibin.jia@demandforce.com>
2018-12-11 16:01:21 +0800

    Add HTTPS support

--- settings/db/initdata.sql
+++ settings/db/initdata.sql
@@ -1,1 +1,1 @@
-insert into ibex.server(name,host_name,http_port,https_port,default_scheme) values ('Upload','10.20.0.95','9999','9999','http');
+insert into ibex.server(name,host_name,http_port,https_port,default_scheme) values ('Upload','10.20.0.95','9999','443','https');
