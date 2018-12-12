d50f24bf20fe4f8f8073527beafe4354092ea033
Justin Jia <jibin.jia@demandforce.com>
2018-12-12 09:05:46 +0800

    Include support tools to HTTPS

--- settings/db/initdata.sql
+++ settings/db/initdata.sql
@@ -1,3 +1,3 @@
 insert into ibex.server(name,host_name,http_port,https_port,default_scheme) values 
-('Upload','10.20.0.95','9999','443','https'),
-('UploadLogs','10.20.0.95','9999','443','https');
+('Upload','10.20.0.95','9999','443','http'),
+('UploadLogs','10.20.0.95','9999','443','http');
