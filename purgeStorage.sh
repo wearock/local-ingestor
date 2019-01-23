6c32b733287eb6fed50ce6e0dfd2d477f18b785f
Justin Jia <jibin.jia@demandforce.com>
2019-01-23 11:25:49 +0800

    Fix support tool deployment by adding new config parameters

--- purgeStorage.sh
+++ purgeStorage.sh
@@ -4,26 +4,26 @@ echo -n "Delete current storage"
 sudo rm -f -r /var/shared/ingestor
 
 echo -n "Recreate root folder"
-mkdir /var/shared/ingestor
+sudo mkdir /var/shared/ingestor
 
 echo -n "Recreate logs folder structure"
-mkdir /var/shared/ingestor/logs
-mkdir /var/shared/ingestor/logs/facade
-mkdir /var/shared/ingestor/logs/translator
-mkdir /var/shared/ingestor/logs/conductor
-mkdir /var/shared/ingestor/logs/ibex-api
-mkdir /var/shared/ingestor/logs/ibconnect
+sudo mkdir /var/shared/ingestor/logs
+sudo mkdir /var/shared/ingestor/logs/facade
+sudo mkdir /var/shared/ingestor/logs/translator
+sudo mkdir /var/shared/ingestor/logs/conductor
+sudo mkdir /var/shared/ingestor/logs/ibex-api
+sudo mkdir /var/shared/ingestor/logs/ibconnect
 
 echo -n "Recreate storage folder structure"
-mkdir /var/shared/ingestor/storage
-mkdir /var/shared/ingestor/storage/sesame
-mkdir /var/shared/ingestor/storage/sesame/system
-mkdir /var/shared/ingestor/storage/sesame/common
-mkdir /var/shared/ingestor/storage/sesame/localstore
-mkdir /var/shared/ingestor/storage/translated
-mkdir /var/shared/ingestor/storage/ibconnect
-mkdir /var/shared/ingestor/storage/d3
-mkdir /var/shared/ingestor/storage/d3/received
-mkdir /var/shared/ingestor/storage/d3/uploaded
+sudo mkdir /var/shared/ingestor/storage
+sudo mkdir /var/shared/ingestor/storage/sesame
+sudo mkdir /var/shared/ingestor/storage/sesame/system
+sudo mkdir /var/shared/ingestor/storage/sesame/common
+sudo mkdir /var/shared/ingestor/storage/sesame/localstore
+sudo mkdir /var/shared/ingestor/storage/translated
+sudo mkdir /var/shared/ingestor/storage/ibconnect
+sudo mkdir /var/shared/ingestor/storage/d3
+sudo mkdir /var/shared/ingestor/storage/d3/received
+sudo mkdir /var/shared/ingestor/storage/d3/uploaded
 
 echo -n "All Done!"
