01756095a08f0e3ee6e38a242346af486848a0f0
Justin Jia <jibin.jia@demandforce.com>
2019-03-22 10:13:50 +0800

    Update DB script path after switching to PostgreSQL 10

--- settings/db/grantaccess.sh
+++ settings/db/grantaccess.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "host all all    10.20.0.0/16   trust">>/var/lib/pgsql/9.6/data/pg_hba.conf
-/usr/pgsql-9.6/bin/pg_ctl reload -D /var/lib/pgsql/9.6/data/
+echo "host all all    10.20.0.0/16   trust">>/data/pg_hba.conf
+/usr/lib/postgresql/10/bin/pg_ctl reload -D /var/lib/pgsql/9.6/data/
