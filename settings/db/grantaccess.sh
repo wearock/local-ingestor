6d27b07c42fcae13b50aa4d0d1c51e374dfe115e
Justin Jia <jibin.jia@demandforce.com>
2019-03-22 10:31:24 +0800

    Fix permission issue of restarting DB service.

--- settings/db/grantaccess.sh
+++ settings/db/grantaccess.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "host all all    10.20.0.0/16   trust">>/data/pg_hba.conf
-/usr/lib/postgresql/10/bin/pg_ctl reload -D /var/lib/pgsql/9.6/data/
+su - postgres -c "PGDATA=$PGDATA /usr/lib/postgresql/10/bin/pg_ctl -w restart"
