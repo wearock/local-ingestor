91a7430e4c875db49fbd05596cdacd6a9cdb2cb2
Justin Jia <jibin.jia@demandforce.com>
2020-08-04 16:26:52 +0800

    Add missing environment file_uploaded_topic

--- restart.sh
+++ restart.sh
@@ -4,8 +4,7 @@ sudo docker restart ibex_api
 sudo docker restart security_manager
 sudo docker restart sm_api
 sudo docker restart translator
-sudo docker restart uploadws_uploader
-sudo docker restart uploadws_processor
+sudo docker restart uploadnode
 sudo docker restart authnode
 sudo docker restart support_tooling
 sudo docker restart ibex-ingestor
